08dcbdf34823eebf1aaa144c33dd406708c22690
girafferz <noiman2004+git@gmail.com>
2025-01-10 14:28:45 +0900

    chore: consolidate eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,32 @@
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import nextPlugin from '@next/eslint-plugin-next';
 
 export default tseslint.config(
   {
-    ignores: ['**/node_modules/**', '**/dist/**', '**/build/**', 'oasys-experiment/**/*']
+    ignores: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/build/**',
+      'oasys-experiment/**/*',
+      'node_modules_linux/**',
+      'playwright/envs.js',
+      'deploy/tools/envs-validator/index.js',
+      'deploy/tools/feature-reporter/build/**',
+      'deploy/tools/feature-reporter/index.js',
+      'public/**',
+      'bridge/**'
+    ]
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
-  eslintConfigPrettier
+  {
+    plugins: {
+      '@next/next': nextPlugin
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules
+    }
+  }
 ); 
